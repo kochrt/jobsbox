a23571339bb1527a746c6a5130b257755287378e
Rob Koch <kochrt@gmail.com>
2016-12-28 16:44:13 -0500

    working on config file

--- Scraper.py
+++ Scraper.py
@@ -1,4 +1,25 @@
 
+'''
+
+post_age: now-30d
+
+num_results: 100
+
+categories:
+  ~category:
+    query:
+      ~string
+    keywords:
+      ~strings
+    email_primary: >
+      ~ ~ ~
+    email_secondary: >
+      ~ ~ ~
+email:
+  
+
+'''
+
 from Models import *
 import requests
 import yaml
@@ -6,37 +27,26 @@ import yaml
 
 def main():
 
-    data = open("config.yml")
+    data = open('config.yml')
     data_map = yaml.load(data)
     data.close()
 
-    print json.dumps(data_map['email'], indent=4)
+    print json.dumps(data_map, indent=4)
 
-    # What we're searching for, separated by 'OR'
-    # synonyms should be in the array
-    query_strings = [
-        ['iOS'],
-        ['Swift'],
-        ['Java'],
-        ['Angular', 'angularjs', 'angular2', 'angular.js'],
-        ['junior'],
-        ['aws']
-    ]
+    query_strings = []
+    categories = {}
+    for category, vals in data_map['categories'].iteritems():
+        query_strings.append(vals['query'])
+        categories[category] = vals['keywords']
 
-    # Words that might be found in a post for a specific role.
-    # Based on how many words of each type we see, modify email accordingly
-    categories = {
-        'ios':      ['ios', 'swift', 'objective-c', 'objective c'],
-        'java':     ['java'],
-        'web':      ['angular', 'angularjs', 'angular js', 'html', 'css', 'javascript'],
-        'aws':      ['aws', 'amazon', 'cloud'],
-        'junior':   ['junior', 'entry']
-    }
+    print 'categories', json.dumps(categories, indent=4)
+    print 'query strings', json.dumps(query_strings, indent=4)
 
     # How old can the posts be
-    time = 'now-30d'
+    time = data_map['post_age']
+    count = data_map['num_results']
 
-    request = Request(query_strings, time=time, count=10)
+    request = Request(query_strings, time=time, count=count)
 
     r = requests.post(
         url='https://search.whoishiring.io/item/item/_search?scroll=10m',
@@ -45,37 +55,37 @@ def main():
 
     data = json.loads(r.text)
 
-    if data["timed_out"]:
-        print("request timed out")
-    elif data["hits"]["total"] < 1:
-        print("nothing found")
+    if data['timed_out']:
+        print('request timed out')
+    elif data['hits']['total'] < 1:
+        print('nothing found')
     else:
-        data = data["hits"]["hits"]
+        data = data['hits']['hits']
 
         for dict in data:
 
-            description = dict["_source"]["description"]
-            company = dict["_source"]["company"]
+            description = dict['_source']['description']
+            company = dict['_source']['company']
 
             # get email
             match = re.search(r'[^@^:\s]+@[^@\s]+\.[a-zA-Z0-9]+', description)
             if match is not None:
                 email = match.group()
             else:
-                email = ""
+                email = ''
 
             print company
             description = strip_all(description)
             counter = CategoryCounter(categories, description.split())
 
-            print "score:", json.dumps(dict["_score"])
+            print 'score:', json.dumps(dict['_score'])
             if len(email) > 0:
                 print email
             print counter.get_counts()
             print
 
             # for html
-            # description = re.sub(r'<[/]?[\w]*>', " ", description)
+            # description = re.sub(r'<[/]?[\w]*>', ' ', description)
 
             # print description
             # print
